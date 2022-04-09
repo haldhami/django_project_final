0aaa48301de75004367c77fb3ca3f79a7e02c7d1
randomrandomer <66445070+randomrandomer@users.noreply.github.com>
2022-04-09 14:15:33 +0100

    updated models and course_details files

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Enrollment, Question, Choice, Submission
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
+
+class QuestionInLine(admin.StackedInline):
+    model = Question
+    extra = 5
+
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 4
 
 
 class LessonInline(admin.StackedInline):
@@ -12,7 +21,7 @@ class LessonInline(admin.StackedInline):
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInLine]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
@@ -22,9 +31,19 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInLine]
+    list_display = ['question_text', 'course']
+    list_filter = ['course']
+    search_fields = ['question_text']
+
+
+
 # <HINT> Register Question and Choice models here
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
